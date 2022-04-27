9fb4c9c0f2093bedc5f8f5ac543d8db6ad1988d4
Alba Rico <amiloyrico.36@gmail.com>
2022-04-26 21:09:43 -0500

    Initial Commit

---
+++ switfUI tutorial/switfUI_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  switfUI_tutorialApp.swift
+//  switfUI tutorial
+//
+//  Created by Alba Yolima Rico Montenegro on 25/04/22.
+//
+
+import SwiftUI
+
+@main
+struct switfUI_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
